d5c98b6022eb420da724e6696367cc4cc990a34d
NickMo <nikolasmoyseos@gmail.com>
2019-11-20 12:11:31 -0500

    Add post user functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,6 +8,30 @@ userController = {
       res.send(results.rows);
     })
     .catch(err => console.error("ERROR getting users: ", err))
+  },
+
+  getSingleUser: (req, res) => {
+    const userId = [req.params.user_id];
+    userModel.getSingleUser(userId)
+    .then(results => {
+      console.log("success getting single user:")
+      res.send(results.rows);
+    })
+    .catch(err => {
+      console.error("ERROR getting single user: ", err)
+    })
+  },
+
+  addUser: (req, res) => {
+    const params = [req.body.first_name, req.body.last_name, req.body.username, req.body.email, req.body.password, req.body.biography, req.body.job_title, req.body.photo]
+    return userModel.addUser(params)
+    .then(() => {
+      console.log("success adding user!")
+      res.sendStatus(201);
+    })
+    .catch(err => {
+      console.error("ERROR adding user", err)
+    })
   }
 }
 
