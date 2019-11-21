fc686b2d2f4a64587f8e94127716099447463ac3
NickMo <nikolasmoyseos@gmail.com>
2019-11-21 15:48:00 -0500

    config for deployment

---
+++ prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    dbHost: process.env.DB_HOST,
+    dbUsername: process.env.DB_USERNAME,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT
+  }
